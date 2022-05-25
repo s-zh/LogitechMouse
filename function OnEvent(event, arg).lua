891c50f5d943f640c45a3f4b79e8a97a597fb7a1
JKJ <1220298025@qq.com>
2022-05-25 22:03:21 +0800

    optimization

--- function OnEvent(event, arg).lua
+++ function OnEvent(event, arg).lua
@@ -1,24 +1,20 @@
 function OnEvent(event, arg)
-    --OutputLogMessage("Event: "..event.." Arg: "..arg.."\n")
+    -- range to response gesture
+    local range = 1000
     -- 多任务：绑定为鼠标4号键(下侧键)按下
     if (event == "MOUSE_BUTTON_PRESSED" and arg == 4) then    
         --OutputLogMessage("key6 is pressed")
-        local old_x, old_y = GetMousePosition()
-        Sleep(100)
-        local new_x, new_y = GetMousePosition()
-        
-        local x = new_x - old_x
-        local y= new_y - old_y
-        --OutputLogMessage("%d, %d\n", x, y)
+        --direction:0->dischange 1->left -1->right 2->up -2->down
+        local direction = getDirection(range)
 
-        if ( ((x >= 0 and x < 2500) or (x <= 0 and x > -2500)) and ((y >= 0 and y < 2500) or (y <= 0 and y > -2500)) ) then
+        if (direction == 0) then
             -- base func: back
             PressKey("lalt")
             PressKey("left")
             ReleaseKey("lalt", "left")
         end    
 
-        if (x < -1000 and ((y >= 0 and y < 2500) or (y <= 0 and y > -2500))) then
+        if (direction == 1) then
             -- 左向手势：下一个桌面
             --OutputLogMessage("enter left\n")
             PressKey("lctrl", "lgui")
@@ -26,7 +22,7 @@ function OnEvent(event, arg)
             ReleaseKey("lctrl", "lgui", "right")
         end    
 
-        if (x > 1000 and ((y >= 0 and y < 2500) or (y <= 0 and y > -2500))) then
+        if (direction == -1) then
             -- 右向手势：上一个桌面
             --OutputLogMessage("enter right\n")
             PressKey("lctrl", "lgui")
@@ -34,7 +30,7 @@ function OnEvent(event, arg)
             ReleaseKey("lctrl", "lgui", "left")
         end
         
-        if (y < -1000 and ((x >= 0 and x < 2500) or (x <= 0 and x > -2500))) then
+        if (direction == 2) then
             -- 上向手势：多任务中心
             --OutputLogMessage("enter up\n")
             PressKey("lgui")
@@ -42,7 +38,7 @@ function OnEvent(event, arg)
             ReleaseKey("lgui", "tab")
         end
         
-        if (y > 1000 and ((x >= 0 and x < 2500) or (x <= 0 and x > -2500))) then
+        if (direction == -2) then
             -- 下向手势：开始菜单
             --OutputLogMessage("enter down\n")
             PressAndReleaseKey("lgui")
@@ -53,30 +49,26 @@ function OnEvent(event, arg)
     -- 快捷操作：绑定为鼠标5号键(上侧键)按下
     if (event == "MOUSE_BUTTON_PRESSED" and arg == 5) then    
         --OutputLogMessage("key5 is pressed")
-        local old_x, old_y = GetMousePosition()
-        Sleep(100)
-        local new_x, new_y = GetMousePosition()
-        
-        local x = new_x - old_x
-        local y= new_y - old_y
-        --OutputLogMessage("%d, %d\n", x, y)
+        --direction:0->dischange 1->left -1->right 2->up -2->down
+        local direction = getDirection(range)
 
-        if ( ((x >= 0 and x < 2500) or (x <= 0 and x > -2500)) and ((y >= 0 and y < 2500) or (y <= 0 and y > -2500)) ) then
+        if (direction == 0) then
             -- base func: forward
             PressKey("lalt")
             PressKey("right")
             ReleaseKey("lalt", "right")
         end    
 
-        if (x < -1000 and ((y >= 0 and y < 2500) or (y <= 0 and y > -2500))) then
-            -- 左向手势：小组件
+        if (direction == 1) then
+            -- 左向手势：powerToys 窗口始终置顶
             --OutputLogMessage("enter left\n")
             PressKey("lgui")
-            PressKey("w")
-            ReleaseKey("lgui", "w")
+            PressKey("lalt")
+            PressKey("e")
+            ReleaseKey("lgui", "lalt", "e")
         end    
 
-        if (x > 1000 and ((y >= 0 and y < 2500) or (y <= 0 and y > -2500))) then
+        if (direction == -1) then
             -- 右向手势：控制中心
             --OutputLogMessage("enter right\n")
             PressKey("lgui")
@@ -84,21 +76,48 @@ function OnEvent(event, arg)
             ReleaseKey("lgui", "a")
         end
         
-        if (y < -1000 and ((x >= 0 and x < 2500) or (x <= 0 and x > -2500))) then
-            -- 上向手势：打开文件夹
+        if (direction == 2) then
+            -- 上向手势：打开资源管理器
             --OutputLogMessage("enter up\n")
             PressKey("lgui")
             PressKey("e")
             ReleaseKey("lgui", "e")
         end
         
-        if (y > 1000 and ((x >= 0 and x < 2500) or (x <= 0 and x > -2500))) then
-            -- 下向手势：搜索
+        if (direction == -2) then
+            -- 下向手势：powerToys Run
             --OutputLogMessage("enter down\n")
             PressKey("lgui")
+            PressKey("lalt")
             PressKey("s")
-            ReleaseKey("lgui", "s")
+            ReleaseKey("lgui", "lalt", "s")
         end
 
     end
 end
+
+
+--direction:0->dischange 1->left -1->right 2->up -2->down
+function getDirection(range)
+    local old_x, old_y = GetMousePosition()
+    Sleep(100)
+    local new_x, new_y = GetMousePosition()
+    local x = new_x - old_x
+    local y= new_y - old_y
+
+    local direction = 0
+    if ( math.abs(x) > math.abs(y) ) then
+        if(x < -range) then 
+            direction = 1
+        elseif(x > range) then
+            direction = -1
+        end    
+    elseif( math.abs(x) < math.abs(y) ) then
+        if(y < -range) then
+            direction = 2
+    elseif(y > range) then
+            direction = -2
+        end        
+    end
+    return direction
+end    
